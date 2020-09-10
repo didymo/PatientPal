ef9697f4e563e6b553edb36364bcb2fee8524d01
Shaun McAuley <shaun.mcauley@thedrop.net.au>
2020-09-10 11:01:45 +1000

    Refactoring the preview component a little more agressively.

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,14 +1,6 @@
-import {Component, NgModule, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {AppComponent} from '../app.component';
-import {Title} from '@angular/platform-browser';
-import {of, Subject, Subscription} from 'rxjs';
-import {Validators} from '@angular/forms';
-import {delay} from 'rxjs/operators';
+import {Component} from '@angular/core';
 import {BuildFormService} from '../_services/build-form.service';
 import {TabView} from '../_classes/TabView';
-import {Assessment} from '../_classes/Assessment';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-
 
 @Component({
     selector: 'app-preview',
@@ -17,45 +9,24 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 })
 /**
  * Handles the previewing of Surveys
- * When a user is editing a survey this component will display what the actual question will look like
+ * When a user is editing a survey this component will display what the actual survey will look like.
  * @author Peter Charles Sims
  */
-export class PreviewComponent implements OnInit {
-
-    tabView: TabView;
-    public onchangefn = new Subject<string>();
+export class PreviewComponent {
 
     /**
-     * Determine if a form is horizontal viewing or not
+     * The currently selected TabView.
+     *
+     * {@type {TabView}
      */
-    public horizontal = true;
-    /**
-     * Defines the width of a label
-     */
-    public labelWidth = 0;
-    public subscriptions: Subscription[] = [];
+    public tabView: TabView;
+
     /**
      * Constructor for PreviewComponent
-     * @param titleService
-     * Used to set the title of the window
      * @param fbService
      * Retrieve surveys from component
      */
-    constructor(
-        private titleService: Title,
-        private fbService: BuildFormService
-    ) {
-    }
-
-    /**
-     * ngOnInit for PreviewComponent
-     */
-    ngOnInit() {
-
+    constructor(private fbService: BuildFormService) {
         this.tabView = this.fbService.getSurvey();
-
-        this.titleService.setTitle('TabviewList | ' + this.tabView.label); // Sets the title
     }
-
-
 }
